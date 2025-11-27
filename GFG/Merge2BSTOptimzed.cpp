31a6ebc41035173a11f7b4b56798b6cebf288149
Sayantan699 <sayantanhalder43@gmail.com>
2025-11-27 19:02:13 +0530

    added

--- GFG/Merge2BSTOptimzed.cpp
+++ GFG/Merge2BSTOptimzed.cpp
@@ -1,5 +1,6 @@
-// TC - O(NlogN + MlogM) SC - O(N + M)
-// SC - O(N + M) for storing inorder traversal of both BSTs
+// TC - O(N + M) SC - O(H1 + H2)
+// SC - O(H1 + H2) for recursion stack space of both BSTs
+
 #include <bits/stdc++.h>
 using namespace std;
 class Node {
@@ -27,18 +28,31 @@ class Solution {
     vector<int> merge(Node *root1, Node *root2) {
         // code here
         
-        vector<int> ans;
-        
+        vector<int> ans1;
+        vector<int> ans2;
         //traversing both the BST using inorder traversal
-        inorder(root1,ans);
-        inorder(root2,ans);
+        inorder(root1,ans1);
+        inorder(root2,ans2);
         
-        sort(ans.begin(),ans.end());
+        vector<int> ans;
+        int i=0,j=0;
+        while(i < ans1.size() && j < ans2.size()){
+            if(ans1[i] < ans2[j])
+                ans.push_back(ans1[i++]);
+            else
+                ans.push_back(ans2[j++]);
+        }
         
+        //for the remaining ele of BST
+        while(i < ans1.size())
+            ans.push_back(ans1[i++]);
+        while(j < ans2.size())
+            ans.push_back(ans2[j++]);
         return ans;
         
     }
 };
+
 int main(){
     return 0;
 }
